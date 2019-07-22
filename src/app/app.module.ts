afb97ec8ce7b1d67fa977c81abfb9083b48132b6
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-22 13:17:20 +0200

    Create app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
-    
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
