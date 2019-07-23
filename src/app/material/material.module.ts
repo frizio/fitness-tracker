20797dbd3c3f3679b903b06991f9cdff43239d20
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-23 10:41:39 +0200

    Add checkbox

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule,
-         MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule,
+            MatCheckboxModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule,
+            MatCheckboxModule]
 })
 export class MaterialModule { }
