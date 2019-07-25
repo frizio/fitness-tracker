4006729394c7984416308dba62f375b885362c05
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-25 10:04:00 +0200

    Add tab component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule,
          MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-         MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
+         MatSidenavModule, MatToolbarModule, MatListModule,
+         MatTabsModule } from '@angular/material';
 
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule,
             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-            MatSidenavModule, MatToolbarModule, MatListModule],
+            MatSidenavModule, MatToolbarModule, MatListModule,
+            MatTabsModule],
   exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule,
             MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-            MatSidenavModule, MatToolbarModule, MatListModule]
+            MatSidenavModule, MatToolbarModule, MatListModule,
+            MatTabsModule]
 })
 export class MaterialModule { }
