64374c1221fe3e787bc5cf5527e389a92bc6a36c
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-29 14:49:48 +0200

    Use rxjs Subscription inside sidenav component

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './../../auth/auth.service';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
 
   @Output()
   closeSidenav = new EventEmitter<void>();
 
-  constructor() { }
+  authSubscription: Subscription;
+  isAuth: boolean;
+
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(
+                              authStatus => {
+                                this.isAuth = authStatus;
+                              }
+    );
   }
 
   onClose() {
     this.closeSidenav.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 
 }
