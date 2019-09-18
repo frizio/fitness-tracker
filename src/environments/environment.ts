8ae7c3d942583e371e2c6fc533d78e29f77530c3
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-09-18 15:11:33 +0200

    Add firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC_qbTF_uyWF18_3bY_LQosdPKsF6P3W4I',
+    authDomain: 'fitness-tracker-backend.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-backend.firebaseio.com',
+    projectId: 'fitness-tracker-backend',
+    storageBucket: 'fitness-tracker-backend.appspot.com',
+    messagingSenderId: '404743557643',
+    appId: '1:404743557643:web:728f92c20c9db4e973ed0e'
+  }
 };
 
 /*
