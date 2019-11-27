e4d98ca44bc8723b1d0b822313e6ae8a4f427a8f
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-27 12:23:49 +0100

    Add production configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC_qbTF_uyWF18_3bY_LQosdPKsF6P3W4I',
+    authDomain: 'fitness-tracker-backend.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-backend.firebaseio.com',
+    projectId: 'fitness-tracker-backend',
+    storageBucket: 'fitness-tracker-backend.appspot.com',
+    messagingSenderId: '404743557643',
+    appId: '1:404743557643:web:728f92c20c9db4e973ed0e'
+  }
 };
